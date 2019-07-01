d183bb490a488acb151dd61f678b105c563c80ce
Ivan Bakaidov <ivan@aacidov.ru>
2019-07-01 17:46:50 +0300

    fix player for spawn

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -1,23 +1,23 @@
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
 
 
 class Player {
   private process: import("child_process").ChildProcess;
 
-  playFile(path: string): Promise<string> {
+  playFile(path: string): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.process = exec("aplay -D bluealsa " + path, (err, res) => {
-        if (err) {
-          reject(err)
-          return;
-        }
-        resolve(res)
+      this.process = spawn("aplay", ["-D bluealsa", path])
+      this.process.on("exit", (code) => {
+        if (code === 0) resolve();
+        this.process = null
+
       })
+      this.process.on('error', reject)
     })
   }
   stop() {
     if (this.process) {
-      this.process.kill('SIGINT')
+      this.process.kill('SIGHUP')
       this.process = null
 
     }
