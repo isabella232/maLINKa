39d339fd01efb03a0238d79a82ffa770d86ca4fb
Ivan Bakaidov <ivan@aacidov.ru>
2019-07-01 17:57:56 +0300

    try change file location

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import keyboard from './keyboard'
 import player from './lib/player';
+import { homedir } from 'os';
 
 keyboard.on('key', (key,date)=>{
   if(key=='a'){
-  player.playFile('~/hello.wav')
+  player.playFile(homedir()+ '/hello.wav')
   .catch(console.error)
   } else{
     player.stop()
