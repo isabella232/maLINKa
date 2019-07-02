7bdf36d207b6f763d09c2e1d3308f3cf766d0a83
Ivan Bakaidov <ivan@aacidov.ru>
2019-07-02 12:20:13 +0300

    fix insert script

--- src/lib/DB/index.ts
+++ src/lib/DB/index.ts
@@ -32,7 +32,7 @@ export class DB {
 
   insert(tableName: string, object: any): Promise<any> {
     const keys = Object.keys(object)
-    return this.query('INSERT INTO ' + tableName + ' (' + keys.map(k => `'${k}'`).join(', ') + ') value (' + (new Array(keys.length).fill('?').join(', ')) + ')', Object.values(object))
+    return this.query('INSERT INTO ' + tableName + ' (' + keys.map(k => `\`${k}\``).join(', ') + ') value (' + (new Array(keys.length).fill('?').join(', ')) + ')', Object.values(object))
   }
   findWhere(tableName: string, query: string, values: string[]): Promise<[any]> {
     return this.query('SELECT * FROM ' + tableName + ' where ' + query, values);
