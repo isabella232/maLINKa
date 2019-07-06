93615375fc3f44c82147e138a357b67f8dfbb5dc
Ivan Bakaidov <ivan@aacidov.ru>
2019-07-06 14:38:23 +0300

    add filter by category

--- src/server/API.ts
+++ src/server/API.ts
@@ -9,18 +9,25 @@ export class API {
   router: Router;
   constructor() {
     this.router = Router();
-    this.router.get('/categories', this.sendAllRows(categoryTable.tableName))
-    this.router.get('/statements', this.sendAllRows(statementTable.tableName))
+    this.router.get('/categories', this.sendRows(categoryTable.tableName))
+    this.router.get('/statements', this.sendRows(statementTable.tableName))
   }
 
 
-  sendAllRows(table: string): import("express-serve-static-core").RequestHandler {
+  sendRows(table: string): import("express-serve-static-core").RequestHandler {
     return async (req, res) => {
       try {
-
-        const rows = await (table == statementTable.tableName ? statementTable.getAllStatements() : categoryTable.getAllCategories());
-
-        res.send(rows);
+        let rows;
+        if (table === categoryTable.tableName) {
+          rows = await categoryTable.getAllCategories()
+        } else {
+          if (req.query.by != null){
+            rows = await statementTable.getStatementsByCategory(parseInt(req.query.by))
+          } else{
+            rows = await statementTable.getAllStatements();
+          }
+        }
+        res.send(rows)
 
       } catch (error) {
         res.status(500).send({ error: error.message })
